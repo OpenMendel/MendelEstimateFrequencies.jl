42386d0175766e4073dcc27f396aa98bd1d70a4c
Eric Sobel <esobel@ucla.edu>
2016-10-06 12:30:29 -0700

    Organized likelihood coding so that the analysis-specific prior, penetrance, and transmission functions are local to the analysis option and then the function names are passed to the likelihood evaluation program.

--- src/MendelEstimateFrequencies.jl
+++ src/MendelEstimateFrequencies.jl
@@ -19,7 +19,6 @@ using DataFrames                        # From package DataFrames.
 using Distributions                     # From package Distributions.
 
 export EstimateFrequencies
-
 """
 This is the wrapper function for the Estimate Allele Frequency analysis option.
 """
@@ -40,7 +39,7 @@ function EstimateFrequencies(control_file = ""; args...)
   # The user specifies the analysis to perform via a set of keywords.
   # Start the keywords at their default values.
   #
-  keyword = set_keyword_defaults!(Dict{ASCIIString, Any}())
+  keyword = set_keyword_defaults!(Dict{AbstractString, Any}())
   #
   # Keywords unique to this analysis should be first defined here
   # by setting their default values using the format:
@@ -90,9 +89,9 @@ function EstimateFrequencies(control_file = ""; args...)
 end # function EstimateFrequencies
 
 function estimate_frequencies_option(pedigree::Pedigree, person::Person,
-  nuclear_family::NuclearFamily, locus::Locus, locus_frame::DataFrame, 
+  nuclear_family::NuclearFamily, locus::Locus, locus_frame::DataFrame,
   phenotype_frame::DataFrame, pedigree_frame::DataFrame,
-  keyword::Dict{ASCIIString, Any})
+  keyword::Dict{AbstractString, Any})
 
   io = keyword["output_unit"]
   skipped_loci = 0
@@ -126,11 +125,12 @@ function estimate_frequencies_option(pedigree::Pedigree, person::Person,
     keyword["parameters"] = locus.alleles[loc]
     keyword["title"] = "Estimate Frequencies analyis for " * locus.name[loc]
     parameter = set_parameter_defaults(keyword)
-    parameter = initialize_optimization(locus, parameter, keyword)
+    parameter =
+      initialize_optimization_estimate_frequencies!(locus, parameter, keyword)
     #
     # Fetch the instructions for conducting the Elston-Stewart algorithm.
     #
-    (instruction, elston_stewart_count) = orchestrate_likelihood(pedigree, 
+    (instruction, elston_stewart_count) = orchestrate_likelihood(pedigree,
       person, nuclear_family, locus, keyword)
     if any(elston_stewart_count .>  keyword["complexity_threshold"])
       println("One or more pedigrees exceeds the complexity threshold.")
@@ -144,8 +144,9 @@ function estimate_frequencies_option(pedigree::Pedigree, person::Person,
     #
     function fun(par)
       copy!(parameter.par, par)
-      f = elston_stewart_loglikelihood(pedigree, person, locus, parameter, 
-        instruction, keyword)
+      f = elston_stewart_loglikelihood(penetrance_estimate_frequencies,
+        prior_estimate_frequencies, transmission_estimate_frequencies,
+        pedigree, person, locus, parameter, instruction, keyword)
       return (f, nothing, nothing)
     end # function fun
     (best_par, best_value) = optimize(fun, parameter)
@@ -156,6 +157,90 @@ function estimate_frequencies_option(pedigree::Pedigree, person::Person,
   end
   show(locus_frame)
   return skipped_loci
-end # function estimate_frequencies_option 
+end # function estimate_frequencies_option
+
+"""
+Supply a penetrance for individual i.
+"""
+function penetrance_estimate_frequencies(person::Person, locus::Locus,
+  multi_genotype::Matrix{Int}, par::Vector{Float64},
+  keyword::Dict{AbstractString, Any}, start::Int, finish::Int, i::Int)
+
+  pen = 1.0
+  return pen
+end # function penetrance_estimate_frequencies
+
+"""
+Supply a prior probability for founder i.
+"""
+function prior_estimate_frequencies(person::Person, locus::Locus,
+  multi_genotype::Matrix{Int}, par::Vector{Float64},
+  keyword::Dict{AbstractString, Any}, start::Int, finish::Int, i::Int)
+
+  prior_prob = 1.0
+  for l = start:finish
+    loc = locus.model_locus[l]
+    allele = multi_genotype[1, l]
+    frequency = par[allele]
+    prior_prob = prior_prob * frequency
+    if !locus.xlinked[loc] || !person.male[i]
+      allele = multi_genotype[2, l]
+      frequency = par[allele]
+      prior_prob = prior_prob * frequency
+    end
+  end
+  return prior_prob
+end # function prior_estimate_frequencies
+
+"""
+Supply the transmission probability that a parent i with a particular
+genotype transmits a particular gamete to his or her child j.
+"""
+function transmission_estimate_frequencies(person::Person, locus::Locus,
+  gamete::Vector{Int}, multi_genotype::Matrix{Int}, par::Vector{Float64},
+  keyword::Dict{AbstractString, Any}, start::Int, finish::Int, i::Int, j::Int)
+  #
+  # For male to male inheritance at an x-linked locus,
+  # set the transmission probability equal to 1.
+  #
+  loc = locus.model_locus[start]
+  xlinked = locus.xlinked[loc]
+  if xlinked && person.male[i] && person.male[j]
+    return 1.0
+  end
+  #
+  # Apply Mendel's segregation rules.
+  #
+  k = multi_genotype[1, start]
+  l = multi_genotype[2, start]
+  m = gamete[start]
+  trans = 0.0
+  if k == m; trans = trans + 0.5; end
+  if l == m; trans = trans + 0.5; end
+  return trans
+end # function transmission_estimate_frequencies
+
+"""
+Initialize the optimization problem.
+"""
+function initialize_optimization_estimate_frequencies!(locus::Locus,
+  parameter::Parameter, keyword::Dict{AbstractString, Any})
+  #
+  # Initialize, bound, and name the parameters.
+  #
+  loc = locus.model_locus[1]
+  for i = 1:parameter.parameters
+    parameter.par[i] = locus.frequency[loc][1, i]
+    parameter.min[i] = 1e-5
+    parameter.name[i] = "freq"*" $i"
+    parameter.name[i] = rpad(parameter.name[i], 8, ' ')
+  end
+  #
+  # Force the parameter frequencies to sum to 1.0.
+  #
+  parameter.constraint[1, :] = 1.0
+  parameter.constraint_level[1] = 1.0
+  return parameter
+end # function initialize_optimization_estimate_frequencies!
 
 end # module MendelEstimateFrequencies
